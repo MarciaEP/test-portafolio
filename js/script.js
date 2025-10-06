0a36b30886eec54cffe58ff308747ebf6fdd1da3
Marcy <marciaedleditsch@gmail.com>
2025-10-06 19:36:23 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+  function mostrarMensaje() {
+            alert("¡Hola estamos funcionando!");
+        }
+
+
+//
+
+const texto = '<input...>\n...\n<input...\n   type="front-end"\n   class="developer"\n   name="Marcy"\n   required>';
+const contenedor = document.getElementById("div-intro");
+
+let i = 0;
+const cursor = document.createElement("span");
+cursor.id = "cursor";
+contenedor.appendChild(cursor);
+
+function escribir() {
+  if (i < texto.length) {
+    cursor.insertAdjacentText("beforebegin", texto[i]);
+    i++;
+    setTimeout(escribir, 100);
+  }
+}
+
+escribir();
