552c8a499f04576d2fd6e8ab995b25415b9c291a
Marcy <marciaedleditsch@gmail.com>
2025-10-07 19:24:16 +0200

    test-2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 const texto = '<label...>\n...\n<input...\n   type="front"\n   class="dev"\n   name="Marcy"\n   required>';
 const contenedor = document.getElementById("div-intro");
+const btnHired = document.getElementById("btn-hired");
 
 let i = 0;
 const cursor = document.createElement("span");
@@ -13,8 +14,14 @@ function escribir() {
     cursor.insertAdjacentText("beforebegin", texto[i]);
     i++;
     setTimeout(escribir, 100);
+  } else {
+    btnHired.style.display = "inline-block"; // Muestra el botón al terminar
+    btnHired.addEventListener("click", () => {
+    window.location.href = "/pages/main.html";
+});
   }
 }
 
 escribir();
+
 
